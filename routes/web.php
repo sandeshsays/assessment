cf0041ad2f2b947ef8b92825141ec8536675d3f5
Sandesh Maharjan <sandeshsays@gmail.com>
2024-03-26 12:42:11 +0545

    list products and their details without using database

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     $viewData = [];
+//     $viewData["title"] = "Home Page - Online Store";
+//     return view('home.index')->with("viewData", $viewData);
+// });
+
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
